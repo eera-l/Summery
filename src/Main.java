c2ad4fb60c8ba80ad4d9a51f705478ebc578e95b
Federica <federica.comuni@gmail.com>
2018-05-07 00:50:24 +0200

    Tidied up folder structure. Added main concept indicator but unfortunately it is too slow and likely won't be used

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import summarizer.Algo;
 
 import java.io.*;
 import java.net.InetSocketAddress;
